5ba3fcbc9f1dfada1bba8b096a9a03edf74fa349
Yuan-Tang Chou <yuantangchou@hotmail.com>
2015-04-21 14:54:20 +0800

    Add parallels job flag

--- FrameworkExe/util/hsg5frameworkReadCxAOD.cxx
+++ FrameworkExe/util/hsg5frameworkReadCxAOD.cxx
@@ -26,8 +26,9 @@ int main(int argc, char* argv[]) {
   std::string submitDir = "submitDir";
   std::string configPath = "data/FrameworkExe/framework-read.cfg";
   if (argc > 1) submitDir = argv[1];
-  if (argc > 2) configPath = argv[2];
-
+  //if (argc > 2) configPath = argv[2];
+  int run(0);
+  if (argc > 2) run = atoi(argv[2]);
   // read run config
   static ConfigStore* config = ConfigStore::createStore(configPath);
 
@@ -48,15 +49,80 @@ int main(int argc, char* argv[]) {
   // choose D4 samples
   std::vector<std::string> sample_names;
   // list all samples here - if they don't exist it will skip
-  sample_names.push_back("monoHbb");
-  sample_names.push_back("Zll");
-  sample_names.push_back("Wlv");
-  sample_names.push_back("singletop");
-  sample_names.push_back("top");
-  sample_names.push_back("Zvv");
+  if(run==0)sample_names.push_back("test");
   //sample_names.push_back("muonData");
   //sample_names.push_back("egammaData");
   //sample_names.push_back("metData");
+  if(run==1)sample_names.push_back("Zvv");
+  if(run==2)sample_names.push_back("Wev");
+  if(run==3)sample_names.push_back("Wmuv");
+  if(run==4)sample_names.push_back("Wtauv");
+  if(run==5)sample_names.push_back("ttbarallhad");
+  if(run==6)sample_names.push_back("ttbarnonall1");
+  if(run==7)sample_names.push_back("ttbarnonall2");
+  if(run==8)sample_names.push_back("dijet1");  
+  //sample_names.push_back("Wlv");
+  if(run==9)sample_names.push_back("dijet2");  
+  if(run==10)sample_names.push_back("singletop");
+  //sample_names.push_back("top");
+  if(run==11){
+  sample_names.push_back("monoWjjIsrDesD1m50");
+  sample_names.push_back("monoWjjIsrDesD1m1300");
+  sample_names.push_back("monoWjjIsrDesD5m50");
+  sample_names.push_back("monoWjjIsrDesD5m1300");
+  sample_names.push_back("monoWjjIsrDesD9m50");
+  sample_names.push_back("monoWjjIsrDesD9m1300");
+  sample_names.push_back("monoWjjIsrConD1m50");
+  sample_names.push_back("monoWjjIsrConD1m1300");
+  sample_names.push_back("monoWjjIsrConD5m50");
+  sample_names.push_back("monoWjjIsrConD5m1300");
+  sample_names.push_back("monoWjjIsrConD9m50");
+  sample_names.push_back("monoWjjIsrConD9m1300");
+  sample_names.push_back("monoZjjIsrD1m50");
+  sample_names.push_back("monoZjjIsrD1m1300");
+  sample_names.push_back("monoZjjIsrD5m50");
+  sample_names.push_back("monoZjjIsrD5m1300");
+  sample_names.push_back("monoZjjIsrD9m50");
+  sample_names.push_back("monoZjjIsrD9m1300");
+  }
+  if(run==12)sample_names.push_back("gamma1");
+  if(run==13)sample_names.push_back("gamma2");
+  if(run==14)sample_names.push_back("gamma3");
+  if(run==15)sample_names.push_back("gamma4");
+  if(run==16)sample_names.push_back("gamma5");
+  if(run==17)sample_names.push_back("gamma6");
+  if(run==18)sample_names.push_back("gamma7");
+  if(run==19)sample_names.push_back("Zee");
+  if(run==20)sample_names.push_back("Zmumu");
+  if(run==21)sample_names.push_back("Ztautau");
+  /*sample_names.push_back("191211");
+  sample_names.push_back("191212");
+  sample_names.push_back("191213");
+  sample_names.push_back("191214");
+  sample_names.push_back("191215");
+  sample_names.push_back("191216");
+  sample_names.push_back("191217");
+  sample_names.push_back("191218");
+  sample_names.push_back("191219");
+  sample_names.push_back("191220");
+  sample_names.push_back("191221");
+  sample_names.push_back("191222");
+  sample_names.push_back("191223");
+  sample_names.push_back("191224");
+  sample_names.push_back("191225");
+  sample_names.push_back("191226");
+  sample_names.push_back("191227");
+  sample_names.push_back("191234");
+  sample_names.push_back("191235");
+  sample_names.push_back("191236");
+  sample_names.push_back("191237");
+  sample_names.push_back("191238");
+  sample_names.push_back("191239");
+  sample_names.push_back("191240");
+  sample_names.push_back("191241");
+  sample_names.push_back("191242");
+  sample_names.push_back("191243");
+  sample_names.push_back("191244");*/
   // sample_names.push_back("ZeeB");
   // sample_names.push_back("ZeeC");
   // sample_names.push_back("ZeeL");
@@ -75,7 +141,7 @@ int main(int argc, char* argv[]) {
   // sample_names.push_back("WtaunuB");
   // sample_names.push_back("WtaunuC");
   // sample_names.push_back("WtaunuL");
-  // sample_names.push_back("ttbar");
+  //sample_names.push_back("ttbar");
   // sample_names.push_back("singletop_t");
   // sample_names.push_back("singletop_s");
   // sample_names.push_back("singletop_Wt");
@@ -86,7 +152,11 @@ int main(int argc, char* argv[]) {
   // datasets copied to afs
   //std::string afsdir("/afs/cern.ch/user/y/ychou/private/monoWZH/Dataset/");
   //std::string afsdir("/afs/cern.ch/work/t/thompson/public/DAOD/v0.0.5/");
-  std::string afsdir("/phys/groups/tev/scratch3/users/ytchou/monoWZ/Dataset_CxAOD/");
+  //std::string afsdir("/phys/groups/tev/scratch3/users/ytchou/monoWZ/Dataset_CxAOD/13TeV_Amelia/");
+  //std::string afsdir("/phys/groups/tev/scratch3/users/ytchou/monoWZ/Dataset_CxAOD/13TeV_arturos/");
+  //std::string afsdir("/phys/groups/tev/scratch3/users/ytchou/monoWZ/Dataset_CxAOD/13TeV_anelson/");
+  std::string afsdir("/phys/groups/tev/scratch3/users/ytchou/monoWZ/Dataset_CxAOD/13TeV_v01/");
+  //std::string afsdir("/phys/groups/tev/scratch3/users/ytchou/monoWZ/Dataset_CxAOD/8TeV/");
   // datasets copied to eos
   TString CxAODver("CxAOD_00-00-05");
   TString xAODver("_p1784");
@@ -133,7 +203,8 @@ int main(int argc, char* argv[]) {
       SH::scanSingleDir (sampleHandler, sample_name, list, "*outputLabel*") ;
     } else {
       SH::DiskListLocal list(sample_dir);
-      SH::scanSingleDir (sampleHandler, sample_name, list, "*outputLabel*") ;
+      //SH::scanSingleDir (sampleHandler, sample_name, list, "*outputLabel*") ;
+      SH::scanSingleDir (sampleHandler, sample_name, list, "*data_CxAOD*") ;
     }
 
     //
